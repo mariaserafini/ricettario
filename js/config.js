1113fef2532c17980133b212ab81b805ad7e5c50
mariaserafini <mariaserafini@gmail.com>
2026-01-08 09:54:50 +0100

    sistemato import foto e testo

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Configurazione Supabase
-const { createClient } = supabase;
-export const _supabase = createClient('https://pbvjzvocwmfpdugktcro.supabase.co', 'sb_publishable_Lq_PBVH3eyzQh7NZdmR39g_sZexqaT7');
-export const app = document.getElementById('app');
-//export const GOOGLE_API_KEY = 'AIzaSyAhGtvCErZcWLnCg1GLXPXJkbjWBaEk7pE';
-export const GEMINI_API_KEY = 'AIzaSyBuJc5LTnwVy_bTXeUGKwxU5LX4LWFlGUQ'; // Sostituisci con la tua chiave API valida
