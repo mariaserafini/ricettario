acbbf2c558fdc227e49854e064c161eab69d8412
mariaserafini <mariaserafini@gmail.com>
2026-03-11 17:23:45 +0100

    sistemata chiave compromessa

--- js/config.js
+++ js/config.js
@@ -2,8 +2,9 @@
 const { createClient } = supabase;
 export const _supabase = createClient('https://pbvjzvocwmfpdugktcro.supabase.co', 'sb_publishable_Lq_PBVH3eyzQh7NZdmR39g_sZexqaT7');
 export const app = document.getElementById('app');
-//export const GOOGLE_API_KEY = 'AIzaSyAhGtvCErZcWLnCg1GLXPXJkbjWBaEk7pE';
-const _p1 = "AIzaSyDLwfdxJ";
-const _p2 = "ohBvXy";
-const _p3 = "SSl2890yIAN0VWdD7o";
-export const GEMINI_API_KEY = _p1 + "-" + _p2 + "-" + _p3;
+
+const _p1 = "AIzaSyDPo3";
+const _p2 = "DiL9rFXcbmy";
+const _p3 = "KhoTKMAKcxkR9k2o";
+export const GEMINI_API_KEY = _p1 + "-" + _p2 + "Q" + _p3;
+
